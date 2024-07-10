8d738df2cfe3a66a9c4f689482e30a412e3b704a
Halil İbrahim <79845872+Halil3509@users.noreply.github.com>
2024-07-10 12:09:18 +0300

    It is a conditional parameter. If you select y, LLM filtering goes on. If not it is cut off after the process of scraping

--- run.py
+++ run.py
@@ -1,21 +1,34 @@
-from scraping import scrape_posts_and_comments, number_of_subreddit
+from scraping import scrape_posts_and_comments
 from llm_filtering import filter_using_llm
 from tqdm import tqdm
+import sys
+
+if len(sys.argv) > 1:
+    # If an argument is provided, use it
+    user_input = sys.argv[1]
+else:
+    # If no argument is provided, prompt the user
+    user_input = input("Do you want to do LLM filtering process or not (y/n): ")
 
 
-# subreddit_names = ["panicdisorder", "selectivemutism", "Anxiety", "generalizedanxiety", "Phobia", "socialanxiety"]
-# "Agoraphobia" was already scraped
+# Define list
+subreddit_names = ["Phobia", "selectivemutism"]
 
-subreddit_names = ["panicdisorder"]
 
 for subreddit_name in tqdm(subreddit_names, desc="Reddit Scraping"):
-    number_of_submissions = number_of_subreddit(subreddit_name)
 
     # Scraping
-    df = scrape_posts_and_comments(subreddit_name=subreddit_name, total_posts=number_of_submissions, posts_per_iteration=100)
-
-    llm_filtered_df = filter_using_llm(df=df)
-
+    df = scrape_posts_and_comments(subreddit_name=subreddit_name)
     df.to_csv(f"Data/{subreddit_name}_pure_en.csv", index=False)
-    llm_filtered_df.to_csv(f"Data/{subreddit_name}_llm_filtered_en.csv", index=False)
-    print(f"{subreddit_name} was saved.")
+    print(f"{subreddit_name} pure data was saved.")
+
+    # If your request limit exceeded. Please select 'n'
+    if user_input == "y":
+        print("Rich guy :), Lets start filtering by using LLM")
+        llm_filtered_df = filter_using_llm(df=df)
+        llm_filtered_df.to_csv(f"Data/{subreddit_name}_llm_filtered_en.csv", index=False)
+        print(f"{subreddit_name} filtered data was saved.")
+    else:
+        print("I hope you can do ASAP. I'm sorry :(")
+
+
